f93cd7561395b5336f4fb949bee915efd6838434
Shubham <shubhamgupto@gmail.com>
2021-10-30 13:39:38 +0530

    removed logger

--- src/main/java/com/example/AddressUpdateApp/sendgrid/MailService.java
+++ src/main/java/com/example/AddressUpdateApp/sendgrid/MailService.java
@@ -5,8 +5,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -26,7 +26,7 @@ import com.sendgrid.helpers.mail.objects.Personalization;
 @Configuration
 @PropertySource("classpath:application.properties")
 public class MailService {
-	private static final Logger logger = LoggerFactory.getLogger(MailService.class);
+//	private static final Logger logger = LoggerFactory.getLogger(MailService.class);
 	
 	@Value("${spring.sendgrid.api-key}") String sendGridAPIKey;
 	public String sendTextEmail() throws IOException {
@@ -47,7 +47,7 @@ public class MailService {
 			request.setEndpoint("mail/send");
 			request.setBody(mail.build());
 			Response response = sg.api(request);
-			logger.info(response.getBody());
+//			logger.info(response.getBody());
 			return response.getBody();
 		} catch (IOException ex) {
 			throw ex;
